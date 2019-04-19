0f8f6c40d036a5328c6bc5217e3ed4a7e1525d93
Imran Ismail <imran.codely@gmail.com>
2019-04-19 17:22:35 +0800

    Finish up payment service

--- src/payment/src/index.js
+++ src/payment/src/index.js
@@ -1,15 +1,58 @@
-const grpc = require('grpc')
-const { PaymentService } = require('./proto');
+const grpc = require("grpc");
+const proto = require("./proto");
+const uuid = require("uuid/v4");
+const sample = require("lodash/sample");
 
-const server = new grpc.Server();
+const PAYMENT_PROVIDERS = ["visa"];
 
-server.addService(PaymentService.service, {
-  Create(call, callback) {
-    console.log(call.request);
-    callback(null, call.request);
+const PAYMENT_STATUS = ["pending", "void", "processing", "success"];
+
+class PaymentServiceImpl {
+  constructor() {
+    this.payments = [];
   }
-})
 
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+  create(payment) {
+    this._assertValidPaymentOption(payment.paymentOption);
 
-server.start()
+    payment.id = uuid();
+    payment.status = sample(PAYMENT_STATUS);
+
+    this.payments.push(payment);
+
+    return payment;
+  }
+
+  _assertValidPaymentOption(paymentOption) {
+    if (!paymentOption) throw new Error("payment option must be specified");
+    if (!PAYMENT_PROVIDERS.includes(paymentOption.provider))
+      throw new Error(`invalid payment provider: ${paymentOption.provider}`);
+    if (
+      paymentOption.ref === "4111111111111111" &&
+      paymentOption.challenge !== "123"
+    )
+      throw new Error("invalid challenge code");
+  }
+}
+
+function main() {
+  const server = new grpc.Server();
+  const impl = new PaymentServiceImpl();
+
+  server.addService(proto.PaymentService.service, {
+    create: async (call, callback) => {
+      try {
+        const payment = await impl.create(call.request);
+        callback(null, payment);
+      } catch (err) {
+        callback(err, null);
+      }
+    }
+  });
+
+  server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
+
+  server.start();
+}
+
+main();
--- src/payment/src/proto.js
+++ src/payment/src/proto.js
@@ -1,15 +1,15 @@
-const path = require('path')
-const grpc = require('grpc')
-const proto = require('@grpc/proto-loader')
+const path = require("path");
+const grpc = require("grpc");
+const proto = require("@grpc/proto-loader");
 
-const PROTO_PATH = path.resolve(__dirname, "../proto/service.proto")
+const PROTO_PATH = path.resolve(__dirname, "../proto/service.proto");
 
 const pkgDefs = proto.loadSync(PROTO_PATH, {
-  keepCase: true,
+  keepCase: false,
   longs: String,
   enums: String,
-  defaults: true,
+  defaults: false,
   oneofs: true
-})
+});
 
-module.exports = grpc.loadPackageDefinition(pkgDefs)
+module.exports = grpc.loadPackageDefinition(pkgDefs);
